154c206838843379fcc07c996e3942afc4e5da85
tamurayy <sweet.clover0503@gmail.com>
2018-12-13 10:10:52 +0900

    Bがキャラ移動処理を追加

--- tanaka.java
+++ tanaka.java
@@ -14,6 +14,9 @@ public class tanaka extends Actor
      */
     public void act() 
     {
-        // Add your action code here.
+        if( Greenfoot.isKeyDown( "right" ) ){
+        setRotation(0);
+        move(1);
+       }
     }    
 }
